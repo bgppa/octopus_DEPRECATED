f914c72b5c15f251b67d1815d677f0889a0a2ab1
Biagio Paparella <paparell@ins.uni-bonn.de>
2019-09-24 14:33:07 +0200

    Simple script for quickly compile the local library

---
+++ mylib/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Compiling the source files in src/ ..." &&
+gcc -c -g -Wextra -Wshadow -pedantic -Wall -Iinclude/ src/*.c &&
+echo "Moving the objects into obj/ ..." &&
+mv *.o obj/ &&
+echo "Done!"
